357d83e56f805469ecd6e70e8e8af466d16549af
Bipro10 <bipro10cse@gmail.com>
2017-05-14 19:05:49 +0600

    Hall Of Fames Of JU Programming Society

--- DevSkill/DCP-46.cpp
+++ DevSkill/DCP-46.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define lng unsigned long long
+#define lng long long
+//#define lng unsigned long long
 
 int main(){
     int n;
@@ -8,8 +9,10 @@ int main(){
     while(n--){
         lng a, b, avg;
         cin>>a>>b;
-        avg= (a/2)+(b/2);
-        if(a&1 && b&1) avg++;
+        if(a>b) swap(a,b);
+        avg= a+(b-a)/2;
+        //avg= (a/2)+(b/2);
+        //if(a&1 && b&1) avg++;
         cout<<avg<<endl;
     }
     return 0;
